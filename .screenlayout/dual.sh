763fdb45993c49207bd1a378ec9f8addd0430d3e
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-01-30 08:39:13 -0300

    Update dual screenlayout

--- .screenlayout/dual.sh
+++ .screenlayout/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x906 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 1366x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x811 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --pos 1920x0 --rotate normal
