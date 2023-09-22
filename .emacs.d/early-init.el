6b6cc3ce86cd7f7ababa1963f7380203df6d094e
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-09-22 15:40:20 -0300

    Add early-init emacs file

---
+++ .emacs.d/early-init.el
@@ -0,0 +1,27 @@
+;;; early-init.el --- Emacs early init -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Early init file for Emacs 27 and above.
+
+;;; Code:
+
+(defconst emacs-start-time (current-time))
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Disable white flash when Emacs starts up
+(set-face-attribute 'default nil :background "#282828" :foreground "#38302F")
+
+;; Makes emacs frame maximized by default (useful for floating window managers systems)
+(push '(fullscreen . maximized) initial-frame-alist)
+(push '(ns-transparent-titlebar . t) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+(provide 'early-init)
+
+;;; early-init.el ends here
