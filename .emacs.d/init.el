c6dcdfb4fe131faa6cfb6caf6bf09752cd6da9d4
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-09-24 19:04:58 -0300

    emacs: Add eat

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -316,6 +316,17 @@
   :config
   (exec-path-from-shell-copy-envs '("PATH")))
 
+(use-package eat
+  :straight
+  (:type git
+         :host codeberg
+         :repo "akib/emacs-eat"
+         :files ("*.el" ("term" "term/*.el") "*.texi"
+                 "*.ti" ("terminfo/e" "terminfo/e/*")
+                 ("terminfo/65" "terminfo/65/*")
+                 ("integration" "integration/*")
+                 (:exclude ".dir-locals.el" "*-tests.el"))))
+
 ;; Magit
 
 (use-package magit
